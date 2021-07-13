e74a60f71a625a4fbc197b22516d32ecf60597c8
sergycg <sergycg@gmail.com>
2021-07-13 20:24:34 +0200

    add heroku Not Idle

--- src/main/java/com/springboot/app/zuul/SpringbootServicioZuulServerApplication.java
+++ src/main/java/com/springboot/app/zuul/SpringbootServicioZuulServerApplication.java
@@ -1,17 +1,35 @@
 package com.springboot.app.zuul;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
+@EnableScheduling
 public class SpringbootServicioZuulServerApplication {
+
+	@Value("${herokuNotIdle.url}")
+	private String herokuNotIdle_url;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioZuulServerApplication.class, args);
 	}
 
+	
+  @Scheduled(cron = "${herokuNotIdle.cronExpression}")
+  public void herokuNotIdle() {
+	System.out.println(" INIT - Heroku not idle execution ");
+	RestTemplate restTemplate = new RestTemplate();
+	ResponseErrorHandler response = restTemplate.getErrorHandler();//exchange("https://zuul-server-heroku.herokuapp.com/", HttpMethod.GET, null, String.class);
+	System.out.println(response);
+	System.out.println(" FINISH - Heroku not idle execution");
+  }
 }
