0614d7bc47ae392b0b573097690552e614f49470
sergycg <sergycg@gmail.com>
2022-03-09 17:11:45 +0100

    v2

--- src/main/java/com/springboot/app/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.springboot.app.zuul.oauth;
 
 import java.util.Arrays;
+import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -53,7 +54,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey(jwtKey);
+		tokenConverter.setSigningKey(Base64.getEncoder().encodeToString(jwtKey.getBytes()));
 		return tokenConverter;
 	}
 
